dffffc11a73c457a73e0b5af83512abb2fd2b01c
Angel Torres <angel.emmanuel.suarez.torrez@ibm.com>
2024-06-27 10:42:37 -0600

    Shader test

--- res/shaders/image.frag
+++ res/shaders/image.frag
@@ -11,6 +11,15 @@ uniform sampler2D image;
 
 void main()
 {
+	int r = int(texture(image, fs_in.texCoord).x * 255.0f);
+	int g = int(texture(image, fs_in.texCoord).y * 255.0f);
+	int b = int(texture(image, fs_in.texCoord).z * 255.0f);
+
+	if (r > 170 && b > 170 && g > 170)
+		color = vec4(0.0, 1.0, 0.0, 1.0);
+	else
+		color = vec4(fs_in.color, 1.0) * texture(image, fs_in.texCoord);
+
 	// color = vec4(fs_in.color, 1.0);
-	color = vec4(fs_in.color, 1.0) * texture(image, fs_in.texCoord);
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,15 @@ int main() {
 
 	Texture txr("../res/ISO_C++_Logo.svg", 1280, 720);
 	Texture txr2("../res/PNG_transparency_demonstration_1.png");
-	Texture txr3("../res/beacon.svg", 1920, 1080);
 
 	Image img(&txr);
 	Image img2(&txr2);
-	Image img3(&txr3);
 
-	std::uint8_t g = 0;
 
 	while (!Engine::windowShouldClose()) {
 
 		Engine::processInput();
-		Engine::clearScreen(255, g, 255);
-		g += 10;
+		Engine::clearScreen(255, 255, 255);
 
 		Image::imageShader->use();
 		Image::imageShader->setValue("projection", Engine::projection);
@@ -31,8 +27,6 @@ int main() {
 
 		img2.setPosition(img2.x + 1, img2.y);
 		img2.draw();
-
-		img3.draw();
 	}
 
 	return Engine::end();
